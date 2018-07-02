23ab66f2b32a2434166790173e7bff78219d8f0f
siddharthasarma <sidduinevitable@gmail.com>
2018-07-02 15:37:10 +0530

    Initial commit :tada:

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'db',
+    connection: {
+      filename: 'postgres://localhost:5433/demo'
+    }
+  }
+};
