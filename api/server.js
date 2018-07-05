206861d4667dcc3c01cbc747aabeb178b9195c48
siddharthasarma <sidduinevitable@gmail.com>
2018-07-05 21:21:51 +0530

    cleanup of code :art:

--- api/server.js
+++ api/server.js
@@ -2,19 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
-const router = express.Router();
-const db = require('../db');
+const router = require('../routes');
 app.use(cors());
 
-router.get('/', (_, res) => {
-  db.getAllStickers().then(stickers => res.json(stickers));
-});
-router.get('/:stickerId', (req, res) => {
-  db.getSticker(req.params.stickerId).then(sticker => res.json(sticker));
-});
-router.delete('/:stickerId', (req, res) => {
-  db.deleteSticker(req.params.stickerId).then(sticker => res.json(sticker));
-});
 app.use('/api/v1/stickers', router);
 
 // Handle 404 and errors
