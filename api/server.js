9ec95ceb0af7be6deb5045171465b310252f9a60
siddharthasarma <sidduinevitable@gmail.com>
2018-07-08 09:34:31 +0530

    Removed body-parser :heavy_minus_sign:

--- api/server.js
+++ api/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8000;
 const router = require('../routes');
 
 // middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use('/api/v1/stickers', router);
 
