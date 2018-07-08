97bfdbfd9c81011760b46337cbca1bb4af949036
siddharthasarma <sidduinevitable@gmail.com>
2018-07-08 09:20:35 +0530

    :hammer: Refactored a lot of code

--- db/index.js
+++ db/index.js
@@ -1,31 +1,36 @@
 const knex = require('knex');
 const config = require('../knexfile');
 const environment = process.env.NODE_ENV || 'development';
-
 const connection = knex(config[environment]);
 
+const getAllStickers = () => {
+  return connection('sticker');
+};
+
+const getSticker = id => {
+  return connection('sticker').where('id', id);
+};
+
+const deleteSticker = id => {
+  return connection('sticker')
+    .where('id', id)
+    .del();
+};
+
+const insertSticker = sticker => {
+  return connection('sticker').insert(sticker, '*');
+};
+
+const updateSticker = (id, sticker) => {
+  return connection('sticker')
+    .where('id', id)
+    .update(sticker, '*');
+};
+
 module.exports = {
-  getAllStickers() {
-    return connection('sticker');
-  },
-
-  getSticker(id) {
-    return connection('sticker').where('id', id);
-  },
-
-  deleteSticker(id) {
-    return connection('sticker')
-      .where('id', id)
-      .del();
-  },
-
-  insertSticker(sticker) {
-    return connection('sticker').insert(sticker, '*');
-  },
-
-  updateSticker(id, sticker) {
-    return connection('sticker')
-      .where('id', id)
-      .update(sticker, '*');
-  }
+  getAllStickers,
+  getSticker,
+  insertSticker,
+  updateSticker,
+  deleteSticker
 };
