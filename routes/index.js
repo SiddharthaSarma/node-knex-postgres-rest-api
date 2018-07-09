b5e7f1d5d58fb03535b56fad4c54060bb00bdf43
siddharthasarma <sidduinevitable@gmail.com>
2018-07-09 22:32:37 +0530

    Added middle to validate the route

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,20 @@ const {
   deleteSticker
 } = require('../controllers');
 
+const isValidId = (req, res, next) => {
+  if (!isNaN(req.params.stickerId)) {
+    return next();
+  }
+  next(new Error('Invalid id'));
+};
+
 router
   .route('/')
   .get(getAllStickers)
   .post(createSticker);
 
 router
+  .use('/:stickerId', isValidId)
   .route('/:stickerId')
   .get(getStickerDetails)
   .put(updateStickerDetails)
